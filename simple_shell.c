8bfb925c4f8c9a26144c8b0d12ed812dc7528abe
Prince <princeasa2005@gmail.com>
2023-10-20 00:15:37 +0000

    Ei, Debzz

--- simple_shell.c
+++ simple_shell.c
@@ -1,8 +1,5 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
-
 /**
  * sig_handler - handles ^C signal interupt
  * @uuv: unused variable
@@ -11,6 +8,7 @@ unsigned int sig_flag;
 
 static void sig_handler(int uuv)
 {
+	unsigned int sig_flag;
 	(void) uuv;
 	if (sig_flag == 0)
 		_puts("\n$ ");
@@ -29,6 +27,7 @@ static void sig_handler(int uuv)
 
 int main(int argc __attribute__((unused)), char **argv, char **environment)
 {
+	unsigned int sig_flag;
 	size_t len_buffer = 0;
 	unsigned int is_pipe = 0, s;
 	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
